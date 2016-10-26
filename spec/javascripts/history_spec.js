af2adf052b1fa5b5d69234981170844fa77ee0fd
Jeffrey Warren <jeff@unterbahn.com>
2016-10-26 17:02:36 -0400

    Selection of changes -- tested (#61)
    * tag module value appending, not overwriting
    
    * wysiwyg image css max-width
    
    * mainImageModule value() reorganiation and better testing
    
    * history limited to 20 items until we optimize; added to test spec too
    
    * history display() test and version bump

--- spec/javascripts/history_spec.js
+++ spec/javascripts/history_spec.js
@@ -75,4 +75,31 @@ describe("History", function() {
   });
 
 
+  it("stores only 20 items until we optimize it", function() {
+
+    editor.history.flush();
+    for (var i = 0; i < 20; i++) {
+      editor.history.add("some text");
+    }
+    expect(editor.history.log.length).toBe(20);
+
+    editor.history.fetch();
+    expect(editor.history.log.length).toBe(20);
+
+  });
+
+
+  it("writes out history to a DOM element", function() {
+
+    $('body').append("<div id='history'></div>");
+
+    editor.history.display($('#history')[0]);
+
+    expect(editor.history.log.length).not.toBe(0);
+    expect($('#history').html()).not.toBe('');
+    expect($('#history p').length).toBe(20);
+
+  });
+
+
 });
