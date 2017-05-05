f439ec84db53b9bdb7e2c6cd599e02a9811dceb3
Jeffrey Warren <jeff@unterbahn.com>
2017-05-05 19:07:58 -0400

    working fetchRelated with adding tags, docs update (#95)

--- src/modules/PublicLab.TitleModule.Related.js
+++ src/modules/PublicLab.TitleModule.Related.js
@@ -47,11 +47,18 @@ module.exports = function relatedNodes(module) {
 
     relatedResults.forEach(function(result) {
 
-      relatedEl.append('<div class="result result-' + result.id + '" style="margin: 3px;"><a class="btn btn-xs btn-default"><i class="fa fa-plus-circle"></i> Add</a> <a class="title"></a> by <a class="author"></a></div>');
+      relatedEl.append('<div class="result result-' + result.id + '" style="margin: 3px;"><a class="btn btn-xs btn-default add-tag"><i class="fa fa-plus-circle"></i> Add</a> <a class="title"></a> by <a class="author"></a></div>');
       relatedEl.find('.result-' + result.id + ' .title').html(result.title);
       relatedEl.find('.result-' + result.id + ' .title').attr('href', result.url);
       relatedEl.find('.result-' + result.id + ' .author').html('@' + result.author);
       relatedEl.find('.result-' + result.id + ' .author').attr('href', '/profile/' + result.author);
+
+      $('.result-' + result.id + ' .add-tag').click(function() {
+        editor.tagsModule.el.find('input').tokenfield('createToken', 'response:' + result.id);
+        // pending https://github.com/publiclab/plots2/issues/646
+        // editor.tagsModule.el.find('input').tokenfield('createToken', 'notify:' + result.author);
+        $('.result-' + result.id).remove();
+      });
 
     });
 
@@ -59,6 +66,7 @@ module.exports = function relatedNodes(module) {
 
   var fetchRelated = module.options.fetchRelated || function fetchRelated() {
 
+    // example
     show([
       { id: 1, title: 'A related post',       url: '/', author: 'eustatic'},
       { id: 2, title: 'Another related post', url: '/', author: 'stevie'},
