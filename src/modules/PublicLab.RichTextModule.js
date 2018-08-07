78e34c9ff6cea533dafde61920f9e88d168ee1f1
Jeffrey Warren <jeff@unterbahn.com>
2018-08-07 18:42:11 -0400

    fixes #114 wild jumping (#151)
    * fixes #114 wild jumping
    
    * fix

--- src/modules/PublicLab.RichTextModule.js
+++ src/modules/PublicLab.RichTextModule.js
@@ -127,14 +127,11 @@ module.exports = PublicLab.RichTextModule = PublicLab.Module.extend({
 
     }
 
-
-    var growTextarea = require('grow-textarea');
-
+    // caused wild jumpy behavior - https://github.com/publiclab/PublicLab.Editor/issues/114
+    //var growTextarea = require('grow-textarea');
     // Make textarea match content height
     _module.resize = function() {
-
-      growTextarea(_module.options.textarea, { extra: 10 });
-
+      //growTextarea(_module.options.textarea, { extra: 10 });
     }
 
     _module.resize();
