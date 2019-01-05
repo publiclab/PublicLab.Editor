3cedd0017bbf4d339859f788aa8373c43edab086
prox07 <rexagod@gmail.com>
2019-01-05 08:24:49 +0530

    PL.Editor #124 indent and remove logs

--- src/modules/PublicLab.RichTextModule.js
+++ src/modules/PublicLab.RichTextModule.js
@@ -224,23 +224,20 @@ module.exports = PublicLab.RichTextModule = PublicLab.Module.extend({
     $(window).scroll(function() {
     var bounding = document.getElementsByClassName('woofmark-mode-markdown')[0].getBoundingClientRect();
 
-      if (
-  bounding.top >= 0 &&
-  bounding.left >= 0 &&
-  bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-  bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-) {
-  wk_c.style.position = "relative";
-  wk_c.style.bottom = 0 + "px";
-  console.log('in')
-} else {
-  wk_c.style.bottom = document.getElementsByClassName('ple-footer')[0].getBoundingClientRect().height + "px";
-  wk_c.style.position = "fixed";
-  wk_c.style.zIndex = 999;
-  console.log('out')
-}
-    })
-
+    if (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+    wk_c.style.position = "relative";
+    wk_c.style.bottom = 0 + "px";
+    } else {
+    wk_c.style.bottom = document.getElementsByClassName('ple-footer')[0].getBoundingClientRect().height + "px";
+    wk_c.style.position = "fixed";
+    wk_c.style.zIndex = 999;
+   }
+  })
 
   }
 
