ffcb496c6be2abf813c7e38f8ef3d72a8426251d
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2019-01-12 20:54:30 +0530

    Add new Map module to add Location . (#211)
    * Mapmodule created
    
    * LBL added
    
    * final commit
    
    * removing error attempt 1
    
    * reverted last commit
    
    * error removed
    
    * added guides and instructions
    
    * UI changes done

---
+++ src/modules/PublicLab.MapModule.js
@@ -0,0 +1,44 @@
+/*
+      MapModule for adding Map .
+      Adds/Removes Tag lat:XX , lon:XX from TagsModule .
+*/
+
+module.exports = PublicLab.MapModule = PublicLab.Module.extend({
+
+  init: function( _editor , options) {
+
+    var _module = this ;
+
+    _module.key = 'map_data' ;
+    _module.options = options || _editor.options.mapModule || {};
+    _module.options.name = 'map' ;
+    _module.options.instructions = 'Add Map with marker to your note' ;
+    _module._super(_editor, _module.options) ;
+    _module.options.required = false;
+
+     var options = {
+       InterfaceOptions: {
+         latId: 'lat',
+         lngId: 'lng'
+       }
+     }
+
+     _module.blurredLocation = new BlurredLocation(options) ;
+
+     _module.blurredLocation.panMapToGeocodedLocation("placenameInput") ;
+     _module.blurredLocation.setBlurred(false) ;
+
+     _module.value = function(){
+       if($("#map_content").is(':visible')){
+        return true ;
+       }
+       else{
+         return false ;
+       }
+     }
+
+     // construct HTML additions
+     _module.build();
+
+  }
+}) ;
