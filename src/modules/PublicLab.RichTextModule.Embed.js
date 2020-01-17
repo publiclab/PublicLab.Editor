9457548cf4e3500056946ea188ae0a1cf9526077
Vladimir <vladimir.mikulic2@gmail.com>
2020-01-17 04:48:37 +0100

    FIX: Null inserted when closing Youtube modal
    Resolves #385

--- src/modules/PublicLab.RichTextModule.Embed.js
+++ src/modules/PublicLab.RichTextModule.Embed.js
@@ -12,8 +12,18 @@ module.exports = function initEmbed(_module, wysiwyg) {
   });
 
   $('.wk-commands .woofmark-command-embed').click(function() {
-    wysiwyg.runCommand(function(chunks, mode) {
-      chunks.before += _module.wysiwyg.parseMarkdown("\n\n\n" + prompt("Enter the full embed code offered by the originating site; for YouTube, that might be: <iframe width='100%' src='https://youtube.com/embed/_________' frameborder='0' allowfullscreen></iframe>") + "\n"); // newlines before and after
+    wysiwyg.runCommand(function (chunks, mode) {
+      var modalResult =
+        "\n\n\n" +
+        prompt(
+          "Enter the full embed code offered by the originating site; for YouTube, that might be: <iframe width='100%' src='https://youtube.com/embed/_________' frameborder='0' allowfullscreen></iframe>"
+        ) +
+        "\n";
+
+      // If a user cancels the modal
+      if (modalResult.trim() === 'null') return;
+
+      chunks.before += _module.wysiwyg.parseMarkdown(modalResult); // newlines before and after
       _module.afterParse(); // tell editor we're done here
     });
 
