a6081f0fc14b29e4a9141e15a1dca29568bbea8e
Kirti <rohirakirti@gmail.com>
2020-03-11 14:17:04 +0530

    Made the table tooltip dismissible on clicking anywhere on the page

--- src/modules/PublicLab.RichTextModule.Table.js
+++ src/modules/PublicLab.RichTextModule.Table.js
@@ -52,7 +52,7 @@ module.exports = function initTables(_module, wysiwyg) {
 
 
   // create a submenu for sizing tables
-  $('.wk-commands').append('<a class="woofmark-command-table btn btn-default " data-toggle="popover" data-trigger="focus" tabindex="0" title="Table <table>"><i class="fa fa-table"></i></a>');
+  $('.wk-commands').append('<a class="woofmark-command-table btn btn-default" data-toggle="popover" data-trigger="focus" tabindex="0" title="Table <table>"><i class="fa fa-table"></i></a>');
 
   $(document).ready(function(){
     $('[data-toggle="table"]').popover();   
