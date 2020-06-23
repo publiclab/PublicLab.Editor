392e40aa0fbca2d2005c54ff78ca64c074f14604
keshav234156 <keshavgarg234156@gmail.com>
2020-06-23 22:03:35 +0530

    fix test

--- dist/PublicLab.Editor.js
+++ dist/PublicLab.Editor.js
@@ -21545,7 +21545,7 @@ PL.MapModule       = require('./modules/PublicLab.MapModule.js');
 
 $(document).ready(function() {
   PL.Util.preventModalScrollToTop();
-  PL.Util.enableModeKeyboardShortcut();
+  PL.Util.enableTextModeKeyboardShortcut();
   PL.Util.preventUploadedImagesDragging();
 });
 
