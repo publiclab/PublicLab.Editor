664bf5ce5330a4affdfa14d597bf743fa8816255
shreyaa-sharmaa <shreyasharma.ss305@gmail.com>
2020-07-06 14:43:27 +0530

    comments added

--- test/ui-testing/center.test.js
+++ test/ui-testing/center.test.js
@@ -7,18 +7,22 @@ beforeAll(async () => {
 
 describe('Center Text', () => {
   test('Centering and decentering text', async () => {
+    // selects a string from textarea in md mode
     await page.setViewport({ width: 1920, height: 1080 });
     await page.waitForSelector('.ple-module-body');
     await page.click('.woofmark-mode-markdown');
     await page.focus('.ple-textarea');
     
+    // center aligns the string and checks for presence of '->' and '<-'
     await page.click('.woofmark-command-autocenter');
     let opening = await page.evaluate(() => document.querySelector('.ple-textarea').value.includes('->'));
     let closing = await page.evaluate(() => document.querySelector('.ple-textarea').value.includes('<-'));
     expect(opening).toBe(true);
     expect(closing).toBe(true);
 
+    // the selection changes from '-> some text <-' to ->'some text'<-
     await page.click('.woofmark-command-autocenter');
+    // clicking again removes the center alignment and tests for absence of '->' and '<-'
     await page.click('.woofmark-command-autocenter');
     opening = await page.evaluate(() => document.querySelector('.ple-textarea').value.includes('->'));
     closing = await page.evaluate(() => document.querySelector('.ple-textarea').value.includes('<-'));
