0daf8cba9a2e6d251ba2704d38235f9eff74a4fe
keshav234156 <45951376+keshav234156@users.noreply.github.com>
2020-07-14 22:05:56 +0530

    Added eslint,lint-staged and Husky (#519)
    * updated privacy link (#413)
    
    Co-authored-by: Cess <cessmbuguar@gmail.com>
    
    * FIX: adds missing entries in .gitignore (#423)
    
    adds .vscode in .gitignore
    adds _SpecRunner.html in .gitignore
    
    Co-authored-by: Emily Ashley <15912063+emilyashley@users.noreply.github.com>
    Co-authored-by: Govind Goel <52847415+govindgoel@users.noreply.github.com>
    Co-authored-by: Cess <cessmbuguar@gmail.com>
    
    * added eslint, Husky and lint-staged
    
    Co-authored-by: Kaira Hiwatari <46723211+anshukaira@users.noreply.github.com>
    Co-authored-by: Cess <cessmbuguar@gmail.com>
    Co-authored-by: Shreya Sharma <55884943+shreyaa-s@users.noreply.github.com>
    Co-authored-by: Emily Ashley <15912063+emilyashley@users.noreply.github.com>
    Co-authored-by: Govind Goel <52847415+govindgoel@users.noreply.github.com>

--- src/modules/PublicLab.Module.js
+++ src/modules/PublicLab.Module.js
@@ -5,7 +5,6 @@
 module.exports = PublicLab.Module = Class.extend({
 
   init: function(_editor, options) {
-
     var _module = this;
 
     _module.options = options || {};
@@ -16,53 +15,41 @@ module.exports = PublicLab.Module = Class.extend({
     _module.el = $('.ple-module-' + _module.options.name);
 
 
-    // Construct and insert HTML, including 
+    // Construct and insert HTML, including
     // instructions, help and tips
-    _module.build = function() {    
-
-      // standard instructions location is at start of ple-module-guide 
+    _module.build = function() {
+      // standard instructions location is at start of ple-module-guide
       _module.el.find('.ple-module-guide')
-                .append('<p class="ple-instructions">' + _module.options.instructions + '</p>')
-                .append('<div class="ple-guide-minor hidden-xs hidden-sm" style = "opacity:0;"></div>');
+          .append('<p class="ple-instructions">' + _module.options.instructions + '</p>')
+          .append('<div class="ple-guide-minor hidden-xs hidden-sm" style = "opacity:0;"></div>');
 
       _module.options.guides.forEach(function(guide) {
-
         _module.el.find('.ple-guide-minor')
-                  .append('<br style="position:absolute;top:' + guide.position + 'px;" class="hidden-xs hidden-sm" />')
-                  .append('<p><i class="fa fa-' + guide.icon + '"></i>' + guide.text + '</p>');
-
+            .append('<br style="position:absolute;top:' + guide.position + 'px;" class="hidden-xs hidden-sm" />')
+            .append('<p><i class="fa fa-' + guide.icon + '"></i>' + guide.text + '</p>');
       });
- 
-    }
+    };
 
 
     // All modules must have a module.valid() method
     // which returns true by default (making them optional).
     // Eventually, we might distinguish between empty and invalid.
     _module.valid = function() {
-
       return true;
-
-    }
+    };
 
 
     // could wrap these in an events() method?
-    _module.el.find('.ple-help-minor').hide(); 
+    _module.el.find('.ple-help-minor').hide();
 
 
-    $(_module.el).mouseenter(function() { 
-
-      _module.el.find('.ple-help-minor').fadeTo(400,1);
-
+    $(_module.el).mouseenter(function() {
+      _module.el.find('.ple-help-minor').fadeTo(400, 1);
     });
 
-    $(_module.el).mouseleave(function() { 
-
-      _module.el.find('.ple-help-minor').fadeTo(400,0);
-      
+    $(_module.el).mouseleave(function() {
+      _module.el.find('.ple-help-minor').fadeTo(400, 0);
     });
-
-
   }
 
 });
