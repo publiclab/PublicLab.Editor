96bbe7338fc9dd2ce4337cef18c38f0ba4d95267
keshav234156 <keshavgarg234156@gmail.com>
2020-08-17 01:47:52 +0530

    fix test

--- test/ui-testing/CustomInsert.test.js
+++ test/ui-testing/CustomInsert.test.js
@@ -7,6 +7,9 @@ beforeAll(async () => {
 
 describe('Custom Insert text', () => {
   test('Add Custom Insert text in rich text mode', async () => {
+    if (await page.evaluate(() => $(".woofmark-mode-markdown").is(":disabled"))) {
+      await page.click('.woofmark-mode-wysiwyg');
+    }
     await page.waitForSelector('.ple-module-body');
     // Click on insert text button
     await page.click('.woofmark-command-insert');
@@ -29,34 +32,36 @@ describe('Custom Insert text', () => {
     await page.waitForSelector('.powertags');
     const stringIsIncluded = await page.evaluate(() => document.querySelector('.wk-wysiwyg').textContent.includes("Power tag: nodes:tag"));
     expect(stringIsIncluded).toBe(true);
-  }, timeout);
 
-  test('Adds strong text in rich text mode', async () => {
-    await page.waitForSelector('.ple-module-body');
-    // Click on Mardown mode button
     await page.waitForSelector('.woofmark-mode-markdown');
     await page.click('.woofmark-mode-markdown');
-    await page.evaluate(() => document.querySelector('.ple-textarea').value += ' ');
+    await page.screenshot({path: 'buddy-screenshot1.png', fullPage: true});
+    await page.waitForSelector('.ple-module-body');
+    // await page.evaluate(() => document.querySelector('.ple-textarea').value += ' ');
     // Click on insert text button
     await page.click('.woofmark-command-insert');
     // Select the option 'Notes' from Menu: What Do you want to insert?
     await page.waitForSelector('.dropdownMenu1');
     await page.click('.dropdownMenu1');
+    await page.screenshot({path: 'buddy-screenshot11.png', fullPage: true});
     await page.waitForSelector('.Notes');
+    await page.screenshot({path: 'buddy-screenshot2.png', fullPage: true});
     await page.click('.Notes');
+    await page.screenshot({path: 'buddy-screenshot3.png'});
     // Select the option 'List' from Menu: Insert as a?
     await page.waitForSelector('.dropdownMenu2');
     await page.click('.dropdownMenu2');
     await page.waitForSelector('.List');
     await page.click('.List');
+    await page.screenshot({path: 'buddy-screenshot4.png'});
     // Add input tag
     await page.$eval('.inputText', (el) => el.value = 'tag');
     // Press the Go! button
     await page.click('.go1');
     await page.click('.woofmark-command-insert');
     // Evaluate the expression
-    const stringIsIncluded = await page.evaluate(() => document.querySelector('.ple-textarea').value.includes('[notes:tag]'));
-    expect(stringIsIncluded).toBe(true);
+    const stringIsIncluded2 = await page.evaluate(() => document.querySelector('.ple-textarea').value.includes('[notes:tag]'));
+    expect(stringIsIncluded2).toBe(true);
     await page.click('.woofmark-mode-wysiwyg');
   }, timeout);
 });
