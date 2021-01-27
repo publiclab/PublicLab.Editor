4dc502323d958b4c8411a6070156189902db928a
jywarren <jeff@unterbahn.com>
2021-01-27 00:51:33 +0000

    jest tweaks

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,5 +3,10 @@ module.exports = {
     headless: process.env.HEADLESS !== 'false',
     slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
     devtools: true
-  }
+  },
+
+  server: {
+    command: 'http-server',
+    port: 8080
+  },
 };
--- test/ui-testing/italic.test.js
+++ test/ui-testing/italic.test.js
@@ -1,8 +1,7 @@
 const timeout = process.env.SLOWMO ? 60000 : 10000;
 const fs = require('fs');
 beforeAll(async () => {
-  path = fs.realpathSync('file://../examples/index.html');
-  await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
+  await page.goto('https://localhost:8080/', {waitUntil: 'domcontentloaded'});
 });
 
 describe('Italic Text', () => {
