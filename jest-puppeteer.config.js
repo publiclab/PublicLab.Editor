e525643adfc46ce13666c142b096c5221f437713
jywarren <jeff@unterbahn.com>
2021-01-27 01:28:49 +0000

    getting closer

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,12 +2,6 @@ module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
     slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
     devtools: true
-  },
-
-  server: {
-    command: 'http-server',
-    port: 8080
-  },
+  }
 };
