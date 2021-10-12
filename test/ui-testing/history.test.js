7d40835ffc7c7fdd9f9dd0e526994a303474b842
Jeffrey Warren <jeff@unterbahn.com>
2021-10-12 12:12:28 -0400

    Create history.test.js

---
+++ test/ui-testing/history.test.js
@@ -0,0 +1,14 @@
+const timeout = process.env.SLOWMO ? 60000 : 10000;
+const fs = require('fs');
+beforeAll(async () => {
+  path = fs.realpathSync('file://../examples/index.html');
+  await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
+});
+
+describe('History functions', () => {
+
+  test('something we are testing described here', () => {
+
+  });
+
+}, timeout);
