0397719e45f8868fa696de132755941eed749303
Jeffrey Warren <jeff@unterbahn.com>
2021-10-12 11:56:43 -0400

    Create publishButton.test.js (#744)
    * Create publishButton.test.js
    
    * Update publishButton.test.js
    
    * Update publishButton.test.js

---
+++ test/ui-testing/publishButton.test.js
@@ -0,0 +1,14 @@
+const timeout = process.env.SLOWMO ? 60000 : 10000;
+const fs = require('fs');
+beforeAll(async () => {
+  path = fs.realpathSync('file://../examples/index.html');
+  await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
+});
+
+describe('Publish button', () => {
+  
+  test('something we are testing described here', () => {
+
+  });
+  
+});
