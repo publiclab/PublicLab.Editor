9d99d4b68c7e6bfafa98fa8de923d0e158f24429
michael-onyeka <65187444+mikemando@users.noreply.github.com>
2021-10-13 05:08:13 -0800

    Update publishButton.test.js

--- test/ui-testing/publishButton.test.js
+++ test/ui-testing/publishButton.test.js
@@ -7,8 +7,18 @@ beforeAll(async () => {
 
 describe('Publish button', () => {
   
-  test('something we are testing described here', () => {
-
-  });
+  test(
+        "publish button gets enabled",
+        () => {
+            // Check initially that Publish button is disabled.
+            expect($(".ple-publish").prop("disabled")).toBe(true);
+            // Add title.
+            $(".ple-module-title input").val("A title");
+            $(".ple-module-title input").keydown();
+            // Check final state of Publish button.
+            expect($(".ple-publish").prop("disabled")).toBe(false);
+        },
+        timeout
+    );
   
 });
