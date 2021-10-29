cfebe5e319bcd6175c86842209eab7885a5b083d
jywarren <jeff@unterbahn.com>
2021-10-29 22:19:34 +0000

    Try using native puppeteer selectors

--- test/ui-testing/publishButton.test.js
+++ test/ui-testing/publishButton.test.js
@@ -3,17 +3,18 @@ const fs = require('fs');
 beforeAll(async () => {
   path = fs.realpathSync('file://../examples/index.html');
   await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
+  await page.waitForSelector('body');
+  await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'});
 });
 
 describe('Publish button', () => {
-    test('publish button gets enabled', () => {
-        // Check initially that Publish button is disabled.
-        expect($('.ple-publish').prop('disabled')).toBe(true);
-        // Add title.
-        $('.ple-module-title input').val('Title');
-        $('.ple-module-title input').keydown();
-        // Check final state of Publish button.
-        expect($('.ple-publish').prop('disabled')).toBe(false);
-    }, timeout);
-
-  });
+  test('publish button gets enabled', () => {
+    // Check initially that Publish button is disabled.
+    expect(page.$('.ple-publish').getAttribute('disabled')).toBe(true);
+    // Add title.
+    page.$('.ple-module-title input').innerText.toBe('Title');
+    page.type('.ple-module-title input', 'hello');
+    // Check final state of Publish button.
+    expect(page.$('.ple-publish').getAttribute('disabled')).toBe(false);
+  }, timeout);
+});
