ca17c06ec025d84dfdd6c7f97a97e40fbf444149
narudesigns <ibeabuchipaul@gmail.com>
2021-12-13 14:57:59 +0100

    got nodejs to recognise jquery command for publishButton jest test file

--- test/ui-testing/publishButton.test.js
+++ test/ui-testing/publishButton.test.js
@@ -1,14 +1,24 @@
 const timeout = process.env.SLOWMO ? 60000 : 10000;
 const fs = require('fs');
-beforeAll(async () => {
-  path = fs.realpathSync('file://../examples/index.html');
-  await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
-});
 
 describe('Publish button', () => {
-  
-  test('something we are testing described here', () => {
+  var $;
+  beforeAll(async () => {
+    path = fs.realpathSync('file://../examples/index.html');
+    await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
 
+    const {JSDOM} = require("jsdom");
+    const myJSDom = new JSDOM(fs.readFileSync(path));
+    $ = require('jquery')(myJSDom.window);
   });
-  
-});
+
+  test('something we are testing described here', () => {
+    // Check initially that Publish button is disabled.
+    expect($('.ple-publish').prop('disabled')).toBe(true);
+    // Add title.
+    $('.ple-module-title input').val('A title');
+    $('.ple-module-title input').keydown();
+    // Check final state of Publish button.
+    expect($('.ple-publish').prop('disabled')).toBe(false);
+  });
+}, timeout);
