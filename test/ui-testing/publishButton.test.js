81777b7d3cb45fcef57d928253bf4cee419cc5b9
Jeffrey Warren <jeff@unterbahn.com>
2021-12-21 14:53:43 -0500

    Update publishButton.test.js

--- test/ui-testing/publishButton.test.js
+++ test/ui-testing/publishButton.test.js
@@ -9,14 +9,14 @@ describe('Publish button', () => {
   
   test('Publish button is enabled', async () => {
     // Check initially that Publish button is disabled.    
-    expect(await page.evaluate(() => document.querySelector('.ple-textarea').disabled).toBe(true);
+    expect(await page.evaluate(() => document.querySelector('.ple-textarea').disabled)).toBe(true);
     // Add title.
     await page.evaluate(() => {
       document.querySelector('.ple-module-title input').value = 'A title';
       document.querySelector('.ple-module-title input').dispatchEvent(new KeyboardEvent('keydown', { "code": "9" })); // random key
     });
     // Check final state of Publish button.
-    expect(await page.evaluate(() => document.querySelector('.ple-textarea').disabled).toBe(false);
+    expect(await page.evaluate(() => document.querySelector('.ple-textarea').disabled)).toBe(false);
   });
 
 });
