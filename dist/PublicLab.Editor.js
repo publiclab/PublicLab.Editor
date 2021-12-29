6d62228a555880a1f7f4849436c0963b5bd18c80
RaviAnand111 <yravianand111@gmail.com>
2021-12-29 23:07:52 +0530

    Revert "Disabled the formatter (#766)"
    This reverts commit 313c3c06beaad6aa76a9bc692201fe4aebb0bdaa.

--- dist/PublicLab.Editor.js
+++ dist/PublicLab.Editor.js
@@ -22475,7 +22475,7 @@ module.exports = function(textarea, _editor, _module) {
     );
     $(".wk-commands, .wk-switchboard").addClass("btn-group");
     $(".wk-commands button, .wk-switchboard button").addClass(
-        "btn btn-light"
+        "btn btn-outline-secondary"
     );
 
     $(".wk-commands a.woofmark-command-insert").addClass("btn-light");
