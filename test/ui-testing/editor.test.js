505a77c911e517a587a0e33e792b274ae3960737
narudesigns <ibeabuchipaul@gmail.com>
2022-01-08 20:22:26 +0100

    added jest mock function

--- test/ui-testing/editor.test.js
+++ test/ui-testing/editor.test.js
@@ -4,8 +4,9 @@ let editor;
 
 beforeAll(async () => {
   path = fs.realpathSync('file://../examples/index.html');
+  // scriptPath = fs.realpathSync('file://../node_modules/jquery/dist/jquery.min.js');
+  // await page.addScriptTag({path: scriptPath});
   await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
-
   await page.evaluate(() => {
     editor = new PL.Editor({
       textarea: document.querySelector('.ple-textarea')
@@ -18,7 +19,7 @@ describe('Errors', () => {
     expect( await page.evaluate(() => document.querySelector('.ple-textarea')) ).toBeDefined();
     expect( await page.evaluateHandle(() => editor) ).toBeDefined();
     expect( await page.evaluateHandle(() => editor.options.textarea) ).toBeDefined();
-    // expect( await page.evaluateHandle(() => editor.options.textarea) ).toEqual(await page.evaluateHandle(() => document.querySelector('.ple-textarea')));
+    expect( await page.evaluateHandle(() => editor.options.textarea.innerHTML) ).toEqual(await page.evaluateHandle(() => document.querySelector('.ple-textarea').innerHTML));
   });
 
   test('counts valid modules and enables publish button', async () => {
@@ -34,5 +35,26 @@ describe('Errors', () => {
     await page.evaluate(() => editor.titleModule.value("My Title")); // set sample title
     await page.evaluate(() => editor.richTextModule.value("My Content")); // set sample content
     expect( await page.evaluate(() => editor.validate()) ).toBe(true);
+  });
+
+  test('Sends AJAX request on editor.publish()', async () => {
+    await page.evaluate(() => editor.options.destination = '/post');
+    await page.evaluate(() => console.log($));
+    const spy = jest.fn().mockImplementation(async (options) => {
+      await page.evaluate(() => {
+        if (options === editor.options.destination) {
+          const d = $.Deferred();
+          d.resolve(options);
+          d.reject(options);
+          return d.promise();
+        }
+      });
+
+      function onPublish(response) {
+        expect(response).toBeDefined();
+      }
+
+      editor.publish(onPublish);
+    });
   });
 });
