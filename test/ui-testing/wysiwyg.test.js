a4e2d9c2e083826ee34b3c0880636ae59104c17d
Paul Ibeabuchi C <ibeabuchipaul@gmail.com>
2022-01-24 15:00:53 +0100

    added try/catch to beforeAll block

--- test/ui-testing/wysiwyg.test.js
+++ test/ui-testing/wysiwyg.test.js
@@ -2,15 +2,19 @@ const timeout = process.env.SLOWMO ? 60000 : 10000;
 const fs = require('fs');
 
 beforeAll(async () => {
-  path = fs.realpathSync('file://../examples/index.html');
-  await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
-  await page.evaluate(() => {
-    wysiwyg = PublicLab.Woofmark(
-        $('.ple-textarea')[0],
-        {options: {}},
-        {options: {}}
-    );
-  });
+  try {
+    path = fs.realpathSync('file://../examples/index.html');
+    await page.goto('file://' + path, {waitUntil: 'domcontentloaded'});
+    await page.evaluate(() => {
+      wysiwyg = PublicLab.Woofmark(
+          $('.ple-textarea')[0],
+          {options: {}},
+          {options: {}}
+      );
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 describe('Wysiwyg', () => {
