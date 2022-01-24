a78a4cc5ec672ba9758d794c0eab1c86bd3a4d02
Paul Ibeabuchi C <ibeabuchipaul@gmail.com>
2022-01-24 17:52:16 +0100

    increased timeout to 30s

--- test/ui-testing/CustomInsert.test.js
+++ test/ui-testing/CustomInsert.test.js
@@ -1,4 +1,4 @@
-const timeout = process.env.SLOWMO ? 60000 : 10000;
+const timeout = process.env.SLOWMO ? 60000 : 30000;
 const fs = require('fs');
 beforeAll(async () => {
   path = fs.realpathSync('file://../examples/index.html');
