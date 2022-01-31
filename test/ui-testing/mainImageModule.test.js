16e42b7f5f34e582da10aca99c28c2738cee931f
Paul Ibeabuchi C <ibeabuchipaul@gmail.com>
2022-01-31 17:58:30 +0100

    converted main_image_module jasmine file to jest test

--- test/ui-testing/mainImageModule.test.js
+++ test/ui-testing/mainImageModule.test.js
@@ -34,11 +34,11 @@ describe('MainImageModule', () => {
   });
 
   test('makes upload request', async () => {
-    const mainImageUrl = 'http://example.com/image.jpg';
-    const nid = 3;
-    const uid = 4;
-
     await page.evaluate(() => {
+      const mainImageUrl = 'http://example.com/image.jpg';
+      const nid = 3;
+      const uid = 4;
+
       editor = new PL.Editor({
         textarea: $('.ple-textarea')[0],
         mainImageModule: {
@@ -50,10 +50,61 @@ describe('MainImageModule', () => {
       _module = editor.mainImageModule;
     });
 
-    expect(await page.evaluate(() => module.el.find('.progress-bar'))).toBeHidden();
+    // neither jasmine nor jest has a toBeHidden() matcher, unless this is custom which I couldn't find
+    // expect(await page.evaluate(() => module.el.find('.progress-bar'))).toBeHidden();
+
+    const spy = jest.fn().mockImplementation(async (options) => {
+      await page.evaluate(() => {
+        if (options.url === '/img') {
+          const d = $.Deferred();
+          d.resolve(options);
+          d.reject(options);
+          return d.promise();
+        }
+      });
+
+      function fileuploadsend(e, data) {
+        expect(data.url).toBe('/img');
+        expect(data.formData.nid).toBe(nid);
+        expect(data.formData.uid).toBe(uid);
+        expect(_module.el.find('.progress-bar')).not.toBeHidden();
+      }
+
+      await page.evaluate(() => {
+        _module.el.find('input').bind('fileuploadsend', fileuploadsend);
+      });
+
+      function fileuploaddone(e, data) {
+        expect(data).not.toBeUndefined();
+      }
+
+      await page.evaluate(() => {
+        _module.el.find('input').bind('fileuploaddone', fileuploaddone);
+      });
+
+      await page.evaluate(() => {
+        module.el.find('input').fileupload('add', {
+          files: [
+            new Blob(["fakedata"])
+          ]
+        });
+      });
+    });
   });
 
-  xtest('xyz', async () => {
+  test('remove image', async () => {
+    await page.evaluate(() => {
+      editor = new PL.Editor({
+        textarea: $('.ple-textarea')[0],
+        mainImageUrl: 'examples/example.gif'
+      });
+      _module = new PL.MainImageModule(editor, {});
+    });
 
+    expect(await page.evaluate(() => _module.value('/image/url.jpg', 34))).toBe('/image/url.jpg');
+    await page.evaluate(() => $('#removeFile').click());
+    expect(await page.evaluate(() => editor.data.has_main_image)).toBe(false);
+    expect(await page.evaluate(() => editor.data.image_revision)).toBe('');
+    expect(await page.evaluate(() => _module.options.url)).toBe('');
   });
 }, timeout);
